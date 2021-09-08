d0d7e144fb6ba6398b9ebc036fa3cb84219734d7
exponenta.euler@https://github.com/arthur-serg/Roguelike.git <exponenta.euler@gmail.com>
2021-09-08 20:38:40 +0500

    internal class CommandList : Player

---
+++ Roguelike/CommandList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roguelike
+{
+    internal class CommandList : Player
+    {
+        public Dictionary<string, Action> Commands = new()
+        {
+            {"up", MoveUp},
+            {"down", MoveDown},
+            {"right", MoveRight},
+            {"left", MoveLeft},
+            {"pick", Pick},
+            {"look", LookAround},
+            {"eat", Eat}
+        };
+        
+        
+
+
+
+
+    }
+}
