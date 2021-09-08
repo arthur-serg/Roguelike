387bfaf163e80cfb4348ec62de16486df8651624
exponenta.euler@https://github.com/arthur-serg/Roguelike.git <exponenta.euler@gmail.com>
2021-09-08 18:57:38 +0500

    level inited

---
+++ Roguelike/Level.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+
+namespace Roguelike
+{
+    internal class Level
+    {
+        //public delegate void GetState();
+
+        public static Random seed = new Random();
+
+        public static int Width { get; private set; } = seed.Next(1, 11);
+        public static int Height { get; private set; } = seed.Next(1, 11);
+
+        public static Room[,] Rooms = new Room[Width, Height];
+
+        Player player = new Player();
+
+        public Level()
+        {
+            
+            player.Position = (Width, Height);
+            
+        }
+
+        public static void GetLevelState() => Console.WriteLine($"{Width} by {Height} level inited.");
+    }
+}
