c8dd1ccdcb32a40cf97765f7d46c897ab9a048f2
exponenta.euler@https://github.com/arthur-serg/Roguelike.git <exponenta.euler@gmail.com>
2021-09-09 13:47:43 +0500

    Base class

---
+++ Roguelike/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    internal class GameObject
+    {
+        public string ID { get; private set; } = "Name";
+        public string Description { get; private set; } = "Description";
+
+        public virtual void ToString() => Console.WriteLine($"{ID}");
+    }
+}
