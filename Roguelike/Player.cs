87f6ecedd480d198e3ea2ddeb52e68e54f5ed0e4
exponenta.euler@https://github.com/arthur-serg/Roguelike.git <exponenta.euler@gmail.com>
2021-09-09 16:04:57 +0500

    Player.Init()

--- Roguelike/Player.cs
+++ Roguelike/Player.cs
@@ -9,21 +9,33 @@ namespace Roguelike
 
         private List<Loot> bag = new(BagCapacityLimit);
         public static int BagCapacityLimit = 100;
-
+        private static (int x, int y) initialPosition = new Room().CurrentRoom;
         public int CurrentBagCapacity { get; set; } = Level.seed.Next(BagCapacityLimit);
 
-        public static (int x, int y) Position { get; set; }
+        public static (int x, int y) Position { get; set; } = initialPosition;
+        //{
+        //    get => (initialPosition.x, initialPosition.y);
+            
+        //    set
+        //    {
+        //        initialPosition.x = value.x;
+        //        initialPosition.y = value.y;
+        //    }
+        //}
+
+
 
         public Player() 
         {
-            Position = (Level.seed.Next(0, Level.X + 1), Level.seed.Next(0, Level.Y + 1));
-            bag.Capacity = CurrentBagCapacity;
             Level.GetLevelState();
             GetPlayerState();
             
         }
 
-        public bool IsStepPossible((int x, int y) position) => position.x >= 0 && position.y <= Level.Y;
+        public void Init(in Room initialPosition)
+        {
+            Position = Player.initialPosition;
+        }
 
         public void GetPlayerState() => Console.WriteLine($"Player is here: {Position} with {CurrentBagCapacity} / {BagCapacityLimit} bag");
 
--- Roguelike/Room.cs
+++ Roguelike/Room.cs
@@ -6,10 +6,13 @@ namespace Roguelike
 {
     internal class Room : GameObject
     {
-        public (int x, int y) RoomPosition { get; set; }
+        public (int x, int y) CurrentRoom { get; set; } = (Level.seed.Next(Level.MeshSize), Level.seed.Next(Level.MeshSize));
+
         public Room()
         {
-            
+            CurrentRoom = (CurrentRoom.x, CurrentRoom.y);
         }
+
+
     }
 }
