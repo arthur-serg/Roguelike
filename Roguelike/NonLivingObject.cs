da6d0bfdbd4af1e74f1bd9680127b2155c0e4483
exponenta.euler@https://github.com/arthur-serg/Roguelike.git <exponenta.euler@gmail.com>
2021-09-09 18:44:01 +0500

    NonLivingObjects

--- Roguelike/NonLivingObject.cs
+++ Roguelike/NonLivingObject.cs
@@ -8,5 +8,25 @@ namespace Roguelike
 {
     class NonLivingObject : GameObject
     {
+        public new string ID { get; private set; } = "NonLivingObject";
+
+        public string Name
+        {
+            get => garbage[Level.seed.Next(0, garbage.Length)];
+
+            private set => garbage[Level.seed.Next(0, garbage.Length)] = value;
+        }
+
+        public string Description { get; private set; }
+
+        public string[] garbage =
+        {
+            "stone",
+            "weed",
+            "glass",
+            "cash",
+            "stem",
+            "charcoal",
+        };
     }
 }
